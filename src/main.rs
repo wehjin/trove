047978868d47139c3c42f89e2998ce7cf3afc7fb
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-05-15 13:12:41 -0700

    Uses yard:title() and yard::trellis().

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,24 @@ use yui::palette::StrokeColor;
 
 fn main() -> Result<(), Box<dyn Error>> {
 	yui::Projector::run_blocking(|projector| {
-		let custodian = yard::textfield("Custodian");
-		let account = yard::textfield("Account");
-		let symbol = yard::textfield("Symbol");
-		let shares = yard::textfield("Shares");
-		let group = yard::textfield("Corral");
-		let price = yard::textfield("Price");
-
-		let rung_count = 6;
-		let ladder_height = 4 * rung_count - 1;
-		let ladder = custodian
-			.pack_bottom(4, account.confine_height(3, Cling::LeftBottom))
-			.pack_bottom(4, symbol.confine_height(3, Cling::LeftBottom))
-			.pack_bottom(4, shares.confine_height(3, Cling::LeftBottom))
-			.pack_bottom(4, group.confine_height(3, Cling::LeftBottom))
-			.pack_bottom(4, price.confine_height(3, Cling::LeftBottom));
-		let title = yard::label("Add Account", StrokeColor::BodyOnBackground, Cling::LeftTop);
+		let strands = vec![
+			yard::textfield("Custodian"),
+			yard::textfield("Account"),
+			yard::textfield("Symbol"),
+			yard::textfield("Shares"),
+			yard::textfield("Corral"),
+			yard::textfield("Price"),
+		];
+		let strand_height = 3;
+		let strand_gap = 1;
+		let trellis_height = (strand_height + strand_gap) * strands.len() as i32 - 1;
+		let trellis = yard::trellis(strand_height, strand_gap, strands);
+		let title = yard::title("Add Asset", StrokeColor::BodyOnBackground, Cling::LeftTop);
 		let button = yard::button("Done", |_| {});
 		let item = yard::empty()
 			.pack_top(3, button.confine(8, 1, Cling::LeftBottom))
-			.pack_top(ladder_height, ladder)
-			.pack_top(3, yard::label("===========", StrokeColor::BodyOnBackground, Cling::LeftTop))
-			.pack_top(1, title)
+			.pack_top(trellis_height, trellis)
+			.pack_top(4, title)
 			.pad(1);
 		projector.set_yard(item);
 	});
