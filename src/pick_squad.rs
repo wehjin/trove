756c1a3e9513d87d2b98f303ebdce34256e15c42
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2020-09-26 09:20:24 -0700

    Renders picked squad.

--- src/pick_squad.rs
+++ src/pick_squad.rs
@@ -1,7 +1,7 @@
 use chad_core::chad::Chad;
 use chad_core::core::Squad;
-use yui::{AfterFlow, ArcYard, Cling, Confine, Create, Flow, Pack, Padding, SenderLink, yard};
-use yui::palette::StrokeColor;
+use yui::{AfterFlow, ArcYard, Before, Cling, Confine, Create, Flow, Pack, Padding, SenderLink, yard};
+use yui::palette::{FillColor, StrokeColor};
 use yui::yard::{ButtonState, Pressable};
 
 use crate::{OWNER, YardId};
@@ -47,14 +47,47 @@ impl yui::Spark for Spark {
 	}
 
 	fn render(state: &Self::State, link: &SenderLink<Self::Action>) -> Option<ArcYard> {
-		let content = yard::label("Empty", StrokeColor::CommentOnBackground, Cling::Center);
+		let mut squads = state.squads.clone();
+		squads.sort_by_key(|it| it.name.to_owned());
+		let selected = match state.pick {
+			Some(id) => squads.iter().position(|it| it.id == id).unwrap_or(0),
+			None => 0,
+		};
+		let content = {
+			if squads.is_empty() {
+				yard::label("Add a squad", StrokeColor::CommentOnBackground, Cling::Center)
+			} else {
+				let name = &squads[selected].name;
+				let title = yard::title(name, StrokeColor::BodyOnPrimary, Cling::LeftBottom);
+				let header = title.pad(1).before(yard::fill(FillColor::Primary));
+				let content = {
+					let unspent = {
+						let label_text = "Unspent: ";
+						let label = yard::label(label_text, StrokeColor::CommentOnBackground, Cling::Left);
+						let button_text = "$0.00K";
+						let button = yard::button(button_text, ButtonState::default(SenderLink::ignore()));
+						yard::empty()
+							.pack_left(button_text.len() as i32 + 6, button)
+							.pack_left(label_text.len() as i32, label)
+					};
+					let members = {
+						let label = yard::label("Members", StrokeColor::CommentOnBackground, Cling::LeftBottom);
+						let list = yard::label("No members", StrokeColor::CommentOnBackground, Cling::Center);
+						let button_text = "Add Member";
+						let button = yard::button(button_text, ButtonState::enabled(SenderLink::ignore()));
+						list.pack_top(1, label)
+							.pack_bottom(3, button)
+					};
+					members.pack_top(3, unspent)
+				}.pad(1);
+				content.pack_top(4, header)
+			}
+		};
 		let side = {
 			let button = yard::button("Add Squad", ButtonState::enabled(link.map(|_| Action::AddSquad)));
 			let content = if state.squads.is_empty() {
 				button.confine_height(3, Cling::Center)
 			} else {
-				let mut squads = state.squads.clone();
-				squads.sort_by_key(|it| it.name.to_owned());
 				let items: Vec<(u8, ArcYard)> = squads.iter().map(|it| {
 					let yard = yard::label(&it.name, StrokeColor::BodyOnBackground, Cling::Center)
 						.pad_cols(1)
@@ -64,10 +97,6 @@ impl yui::Spark for Spark {
 						}));
 					(3, yard)
 				}).collect();
-				let selected = match state.pick {
-					Some(id) => squads.iter().position(|it| it.id == id).unwrap_or(0),
-					None => 0,
-				};
 				let list = yard::list(YardId::PickSquadList.as_i32(), selected, items);
 				list.pack_bottom(3, button)
 			};
