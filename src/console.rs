18de9db7bbf4d28ea4c29305abc460d73b4f5048
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-02-22 20:32:14 -0800

    Playing with bevy.

--- src/console.rs
+++ src/console.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::io::stdout;
 
+use bevy::prelude::Resource;
 use crossterm::{execute, terminal};
 use crossterm::cursor::MoveTo;
 use crossterm::style::{Color, Print, SetBackgroundColor, SetForegroundColor};
@@ -13,33 +14,64 @@ use crossterm::terminal::EnableLineWrap;
 use crossterm::terminal::EnterAlternateScreen;
 use crossterm::terminal::LeaveAlternateScreen;
 
-pub(crate) struct Console;
+use crate::Position;
+
+#[derive(Resource)]
+pub struct Console;
 
 impl Console {
+	pub fn move_print(&self, col: u16, row: u16, msg: &str) {
+		execute!(
+			stdout(),
+			MoveTo(col, row),
+			SetForegroundColor(Color::Yellow),
+			SetBackgroundColor(Color::Black),
+			Print(msg),
+		).expect("moveto, print");
+	}
+	pub fn color(&self, pos: &Position, color: Color) {
+		let width = pos.right - pos.left;
+		let spaces = (0..width).map(|_| ' ').collect::<String>();
+		let col = pos.left;
+		for row in pos.top..pos.bottom {
+			execute!(
+				stdout(),
+				MoveTo(col, row),
+				SetBackgroundColor(color),
+				Print(&spaces),
+			).expect("moveto, print");
+		}
+	}
+
+	pub fn print(&self, msg: &str) {
+		let (cols, rows) = terminal::size().expect("size");
+		let title = format!(" {rows} x {cols} {msg} ");
+		let underline = (0..title.len()).map(|_| '▀').collect::<String>();
+		execute!(
+			stdout(),
+			SetForegroundColor(Color::Blue),
+			SetBackgroundColor(Color::Black),
+			MoveTo(10,2),
+			Print(&title),
+			SetForegroundColor(Color::Green),
+			SetBackgroundColor(Color::Black),
+			MoveTo(10,3 ),
+			Print(underline),
+			MoveTo(10,5 ),
+		).expect("execute");
+	}
 	pub fn start() -> Result<Self, Box<dyn Error>> {
-		let (cols, rows) = terminal::size()?;
 		enable_raw_mode()?;
-		let msg = format!(" {rows} x {cols} chad! ");
-		let upper_half = "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀";
 		execute!(
 			stdout(),
 			EnterAlternateScreen,
 			DisableLineWrap,
 			SetBackgroundColor(Color::Black),
 			Clear(ClearType::All),
-			SetForegroundColor(Color::Blue),
-			SetBackgroundColor(Color::Black),
-			MoveTo(10,2),
-			Print(&msg),
-			SetForegroundColor(Color::Green),
-			SetBackgroundColor(Color::Black),
-			MoveTo(10,3 ),
-			Print(upper_half),
-			MoveTo(10,5 ),
 		)?;
 		Ok(Self)
 	}
-	fn exit(&mut self) -> Result<(), Box<dyn Error>> {
+	pub fn stop() -> Result<(), Box<dyn Error>> {
 		execute!(
 			stdout(),
 			Clear(ClearType::All),
@@ -50,10 +82,3 @@ impl Console {
 		Ok(())
 	}
 }
-
-impl Drop for Console {
-	fn drop(&mut self) {
-		self.exit().expect("exit does not fail");
-	}
-}
-
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,59 @@
 use std::error::Error;
 
+use bevy::prelude::{App, Commands, Component, IntoSystemConfigs, Query, Res, Startup, Update, With};
 use crossterm::event::read;
+use crossterm::style::Color;
 
 use console::Console;
 
 fn main() -> Result<(), Box<dyn Error>> {
-	let _terminal = Console::start()?;
-	read()?;
+	Console::start()?;
+	App::new()
+		.add_systems(Startup, add_console)
+		.add_systems(Startup, add_panels.after(add_console))
+		.add_systems(Update, hello_world)
+		.add_systems(Update, greet_panels.after(hello_world))
+		.add_systems(Update, await_human.after(greet_panels))
+		.run();
+	Console::stop()?;
 	Ok(())
 }
 
-mod console;
+fn add_console(mut commands: Commands) {
+	let console = Console::start().expect("start");
+	commands.insert_resource(console);
+}
+
+fn await_human() {
+	read().expect("read");
+}
+
+fn hello_world(console: Res<Console>) {
+	console.move_print(5, 0, "hello world!");
+}
+
+fn add_panels(mut commands: Commands) {
+	commands.spawn((Panel, Position { left: 0, top: 15, right: 20, bottom: 20, near: 0, far: 0 }));
+	commands.spawn((Panel, Position { left: 35, top: 5, right: 40, bottom: 40, near: 0, far: 0 }));
+}
+
+fn greet_panels(console: Res<Console>, query: Query<&Position, With<Panel>>) {
+	for pos in &query {
+		console.color(pos, Color::Green);
+	}
+}
+
+#[derive(Component)]
+struct Panel;
+
+#[derive(Component, Copy, Clone, Eq, PartialEq)]
+pub struct Position {
+	left: u16,
+	top: u16,
+	right: u16,
+	bottom: u16,
+	near: u16,
+	far: u16,
+}
+
+pub mod console;
