8ef21f41f963b95eac6fa7aaada29382a5d605cc
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-02-26 18:42:01 -0800

    Moves remaining painters from [sample] into [painters].

--- src/tools/painters.rs
+++ src/tools/painters.rs
@@ -1,6 +1,6 @@
 use crate::components::fill::Fill;
 use crate::resources::solar_dark;
-use crate::tools::fill::Glyph;
+use crate::tools::fill::{Glyph, string_to_fills};
 use crate::tools::Painter;
 use crate::tools::zrect::ZRect;
 
@@ -19,5 +19,47 @@ impl Painter for TodoPainter {
 	fn paint(&self) -> Vec<Fill> {
 		let fill = Fill { glyph: Glyph::Solid(solar_dark::MAGENTA), volume: self.0 };
 		vec![fill]
+	}
+}
+
+pub struct ColorIndex(pub usize);
+
+pub struct ButtonPainter {
+	pub zrect: ZRect,
+	pub label: String,
+	pub label_color: ColorIndex,
+	pub base_color: ColorIndex,
+}
+
+impl Painter for ButtonPainter {
+	fn paint(&self) -> Vec<Fill> {
+		let fills1 = string_to_fills(&self.label, self.zrect.move_closer(1), self.label_color.0);
+		let fills2 = vec![Fill { glyph: Glyph::Solid(self.base_color.0), volume: self.zrect }];
+		vec![fills1, fills2].into_iter().flatten().collect()
+	}
+}
+
+pub struct StringPainter {
+	pub zrect: ZRect,
+	pub string: String,
+	pub string_color: ColorIndex,
+}
+
+impl Painter for StringPainter {
+	fn paint(&self) -> Vec<Fill> {
+		let zrect = self.zrect;
+		string_to_fills(&self.string, zrect, self.string_color.0)
+	}
+}
+
+pub struct TitlePainter(pub ZRect);
+
+impl Painter for TitlePainter {
+	fn paint(&self) -> Vec<Fill> {
+		let mut vec = vec![
+			Fill { glyph: Glyph::Solid(solar_dark::BASE02), volume: self.0 },
+		];
+		vec.extend(string_to_fills("hello world!", self.0, solar_dark::BASE1));
+		vec
 	}
 }
--- src/tools/sample.rs
+++ src/tools/sample.rs
@@ -1,10 +1,8 @@
-use crate::components::fill::Fill;
 use crate::resources::solar_dark;
 use crate::systems::ViewEffects;
-use crate::tools::{BoxRender, Painter, Shaper, ShapingResult, ViewUpdating};
-use crate::tools::fill::{Glyph, string_to_fills};
+use crate::tools::{BoxRender, Shaper, ShapingResult, ViewUpdating};
 use crate::tools::inset::Inset;
-use crate::tools::painters::BodyPanelPainter;
+use crate::tools::painters::{BodyPanelPainter, ButtonPainter, ColorIndex, StringPainter, TitlePainter};
 use crate::tools::ViewStarting;
 use crate::tools::zrect::ZRect;
 
@@ -56,47 +54,5 @@ impl Shaper for MyShaper {
 			];
 			ShapingResult::SetPainters(painters)
 		}
-	}
-}
-
-pub struct ColorIndex(pub usize);
-
-pub struct ButtonPainter {
-	zrect: ZRect,
-	label: String,
-	label_color: ColorIndex,
-	base_color: ColorIndex,
-}
-
-impl Painter for ButtonPainter {
-	fn paint(&self) -> Vec<Fill> {
-		let fills1 = string_to_fills(&self.label, self.zrect.move_closer(1), self.label_color.0);
-		let fills2 = vec![Fill { glyph: Glyph::Solid(self.base_color.0), volume: self.zrect }];
-		vec![fills1, fills2].into_iter().flatten().collect()
-	}
-}
-
-pub struct StringPainter {
-	zrect: ZRect,
-	string: String,
-	string_color: ColorIndex,
-}
-
-impl Painter for StringPainter {
-	fn paint(&self) -> Vec<Fill> {
-		let zrect = self.zrect;
-		string_to_fills(&self.string, zrect, self.string_color.0)
-	}
-}
-
-struct TitlePainter(ZRect);
-
-impl Painter for TitlePainter {
-	fn paint(&self) -> Vec<Fill> {
-		let mut vec = vec![
-			Fill { glyph: Glyph::Solid(solar_dark::BASE02), volume: self.0 },
-		];
-		vec.extend(string_to_fills("hello world!", self.0, solar_dark::BASE1));
-		vec
 	}
 }
