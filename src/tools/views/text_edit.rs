2787b93a167c7de2cf5926c340d720cc8d3d5390
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-03-13 09:51:47 -0700

    Made [cursor_index] mandatory in TextEditor.

--- src/tools/views/text_edit.rs
+++ src/tools/views/text_edit.rs
@@ -3,7 +3,7 @@ use crate::tools::frame::Frame;
 pub struct TextEditor {
 	pub text: String,
 	pub frame: Frame,
-	pub cursor_index: Option<usize>,
+	pub cursor_index: usize,
 	pub left_index: usize,
 }
 
@@ -12,12 +12,12 @@ impl TextEditor {
 		Self {
 			text: "".to_string(),
 			frame: Frame::default(),
-			cursor_index: None,
+			cursor_index: 0,
 			left_index: 0,
 		}
 	}
 	pub fn with_text(self, text: String) -> Self {
-		let new_cursor_index = Self::normalize_cursor_index(&text, self.cursor_index);
+		let new_cursor_index = Self::fit_cursor_to_text(&text, self.cursor_index);
 		let new_left_index = Self::normalize_left_index(&text, self.frame, new_cursor_index, self.left_index);
 		Self {
 			text,
@@ -36,8 +36,8 @@ impl TextEditor {
 		}
 	}
 
-	pub fn with_cursor_index(self, cursor_index: Option<usize>) -> Self {
-		let new_cursor_index = Self::normalize_cursor_index(&self.text, cursor_index);
+	pub fn with_cursor_index(self, cursor_index: usize) -> Self {
+		let new_cursor_index = Self::fit_cursor_to_text(&self.text, cursor_index);
 		let new_left_index = Self::normalize_left_index(&self.text, self.frame, new_cursor_index, self.left_index);
 		Self {
 			text: self.text,
@@ -57,11 +57,11 @@ impl TextEditor {
 		}
 	}
 
-	fn normalize_cursor_index(text: &String, cursor_index: Option<usize>) -> Option<usize> {
+	fn fit_cursor_to_text(text: &String, cursor_index: usize) -> usize {
 		let text_len = text.chars().count();
-		cursor_index.map(|i| i.min(text_len + Self::CURSOR_LEN))
+		cursor_index.min(text_len + Self::CURSOR_LEN)
 	}
-	fn normalize_left_index(text: &String, frame: Frame, cursor_index: Option<usize>, left_index: usize) -> usize {
+	fn normalize_left_index(text: &String, frame: Frame, cursor_index: usize, left_index: usize) -> usize {
 		let left_index = {
 			let text_len = text.chars().count();
 			let excess_len = (text_len + Self::CURSOR_LEN) as isize - frame.width() as isize;
@@ -71,13 +71,9 @@ impl TextEditor {
 				left_index.min(excess_len as usize).min(text_len + Self::CURSOR_LEN)
 			}
 		};
-		if let Some(cursor_index) = cursor_index {
-			let min_left_index = (cursor_index as isize - frame.width() as isize + 1).max(0) as usize;
-			let max_left_index = (cursor_index as isize + frame.width() as isize - 1).max(0) as usize;
-			left_index.max(min_left_index).min(max_left_index)
-		} else {
-			left_index
-		}
+		let min_left_index = (cursor_index as isize - frame.width() as isize + 1).max(0) as usize;
+		let max_left_index = (cursor_index as isize + frame.width() as isize - 1).max(0) as usize;
+		left_index.max(min_left_index).min(max_left_index)
 	}
 
 	const CURSOR_LEN: usize = 1;
